de6cb802553cbd7d1647458c9c6411286523f40b
Alexander Nadeau <wareya@gmail.com>
2025-11-16 05:44:05 -0500

    Update parallax shader calculations and shadowing
    Refactor parallax calculations and shadowing logic in GLSL.

--- EXPERIMENTS/_snippet_parallax.glsl
+++ EXPERIMENTS/_snippet_parallax.glsl
@@ -1,7 +1,7 @@
 
+// given as it would be done in the terrain shader:
 #if @parallax
 
-    //vec3 eyeTexSpace = transpose(normalToViewMatrix) * normalize(-passViewPos);
     vec3 eyeTexSpace = normalize(-passViewPos) * normalToViewMatrix;
     float height = texture2D(normalMap, adjustedUV).a; // 0 : deep. 1 : surface.
     vec2 offset = getParallaxOffset(eyeTexSpace, height);
@@ -47,3 +47,31 @@
     adjustedUV += offset;
     
 #endif
+
+
+// .........
+// Shadow offset
+
+    float shadowing = 1.0;
+    if (gl_FragData[0].a > 0.0)
+    {
+    #if @parallax
+        vec3 uvWorld = normalToViewMatrix * vec3(origAdjustedUV, 0.0);
+        float _x1 = length(dFdx(uvWorld));
+        float _y1 = length(dFdy(uvWorld));
+        float _x2 = length(dFdx(passViewPos.xyz));
+        float _y2 = length(dFdy(passViewPos.xyz));
+        float _d = length(vec2(_x2, _y2))/length(vec2(_x1, _y1));
+        
+        vec3 _offs = vec3(offset.x, -offset.y, 0.0) * _d;
+        _offs = normalToViewMatrix * _offs;
+        shadowing = unshadowedLightRatioOffset(adjustedDepth, _offs);
+    #else
+        shadowing = unshadowedLightRatio(adjustedDepth);
+    #endif
+    }
+
+// ... in shadow fragment shader, make a `float unshadowedLightRatioOffset(float distance, vec3 coord_offset)` that does:
+vec4 offs = shadowSpaceMatrix@shadow_texture_unit_index * vec4(coord_offset, 0.0);
+// ... use as
+shadowSpaceCoords@shadow_texture_unit_index + offs
